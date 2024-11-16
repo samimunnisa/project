e1e2cc1ec7d3d3ebda15e21c7b98ad71004566f4
samimunnisa <samimunnisamohammad16@gmail.com>
2024-11-16 14:50:37 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,19 @@
+import java.lang.*
+import java.util.Scanner;
+class Greatest
+
+{
+ public static void main(String[] args)
+    {
+     int num =0;
+     if (num % 2 == 0)
+     {
+       System.out.println("Entered Number is Even");
+     }
+ 
+        else
+     {
+      System.out.println("Entered Number is Odd");
+     }
+    }
+}
